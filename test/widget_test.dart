d77f816ca00b77d9d74d162b7161b8e6bf1a4472
Jan Michael <kwienyu@gmail.com>
2025-09-08 16:49:25 +0800

    modify app icon and app name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:glamourapp/main.dart';
+import 'package:Glamour/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
